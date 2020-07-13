489f01aad6716e04e0d2faa816e5bae5a2e16362
Marlon Luan <marlonluan@gmail.com>
2020-07-13 17:44:04 -0300

    Update Test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 import { strict as assert } from 'node:assert';
 import binCheck from 'bin-check';
 import { test } from 'uvu';
-import hugoBin from '../index.js';
+import hugoBinExtended from '../index.js';
 
 test('should return path to binary and work', () => {
-  return binCheck(hugoBin, ['version']).then(works => {
+  return binCheck(hugoBinExtended, ['version']).then(works => {
     assert.ok(works);
   });
 });
