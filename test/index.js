63f4d4f0b539849a7f210d4e215bf7fb3c75dd59
Marlon Luan <marlonluan@gmail.com>
2020-07-13 17:44:04 -0300

    Update Test

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ import { strict as assert } from 'node:assert';
 import process from 'node:process';
 import binCheck from 'bin-check';
 import { test, suite } from 'uvu';
-import hugoBin from '../index.js';
+import hugoBinExtended from '../index.js';
 import hugoLib from '../lib/index.js';
 
 test('should return path to binary and work', () => {
-  return binCheck(hugoBin, ['version']).then(works => {
+  return binCheck(hugoBinExtended, ['version']).then(works => {
     assert.ok(works);
   });
 });
@@ -17,38 +17,20 @@ test.run();
  * Verify Custom/Enterprise Repository overwrite.
  */
 
-const hugoLibCustomRepoTestSuite = suite('hugo-bin overwrite download repository');
+const hugoLibCustomRepoTestSuite = suite('hugo-bin-extended overwrite download repository');
 
 hugoLibCustomRepoTestSuite.before.each(() => {
   // Ensure that the environment is cleaned before next test run.
-  delete process.env.npm_config_hugo_bin_build_tags;
-  delete process.env.npm_config_hugo_bin_download_repo;
+  delete process.env.npm_config_hugo_bin_extended_download_repo;
 });
 
 hugoLibCustomRepoTestSuite('verify test env', () => {
-  assert.equal(process.env.npm_config_hugo_bin_build_tags, undefined);
-  assert.equal(process.env.npm_config_hugo_bin_download_repo, undefined);
+  assert.equal(process.env.npm_config_hugo_bin_extended_download_repo, undefined);
 })
 
 // Default Repository - Test Cases
 
-hugoLibCustomRepoTestSuite('should return default repository url - Repository: default - Extended: undefined', () => {
-  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
-  repoSources.forEach((sourceUrl) => {
-    assert.ok(sourceUrl.startsWith('https://github.com/'));
-  });
-});
-
-hugoLibCustomRepoTestSuite('should return default repository url - Repository: default - Extended: empty', () => {
-  process.env.npm_config_hugo_bin_build_tags = '';
-  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
-  repoSources.forEach((sourceUrl) => {
-    assert.ok(sourceUrl.startsWith('https://github.com/'));
-  });
-});
-
-hugoLibCustomRepoTestSuite('should return default repository url - Repository: default - Extended: extended', () => {
-  process.env.npm_config_hugo_bin_build_tags = 'extended';
+hugoLibCustomRepoTestSuite('should return default repository url - Repository: default', () => {
   const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
   repoSources.forEach((sourceUrl) => {
     assert.ok(sourceUrl.startsWith('https://github.com/'));
@@ -57,29 +39,11 @@ hugoLibCustomRepoTestSuite('should return default repository url - Repository: d
 
 // Custom/Enterprise Repository Test Cases
 
-hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom - Extended: undefined', () => {
-  process.env.npm_config_hugo_bin_download_repo = 'https://some1.example.com';
+hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom', () => {
+  process.env.npm_config_hugo_bin_extended_download_repo = 'https://some1.example.com';
   const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
   repoSources.forEach((sourceUrl) => {
     assert.ok(sourceUrl.startsWith('https://some1.example.com/'));
-  });
-});
-
-hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom - Extended: empty', () => {
-  process.env.npm_config_hugo_bin_build_tags = '';
-  process.env.npm_config_hugo_bin_download_repo = 'https://some2.example.com';
-  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
-  repoSources.forEach((sourceUrl) => {
-    assert.ok(sourceUrl.startsWith('https://some2.example.com/'));
-  });
-});
-
-hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom - Extended: extended', () => {
-  process.env.npm_config_hugo_bin_build_tags = 'extended';
-  process.env.npm_config_hugo_bin_download_repo = 'https://some3.example.com';
-  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
-  repoSources.forEach((sourceUrl) => {
-    assert.ok(sourceUrl.startsWith('https://some3.example.com/'));
   });
 });
 
